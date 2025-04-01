b4b6d6d66a3846929fa34c1a964159dfc0a1ccd4
ShrikeGames <ShrikeGames@gmail.com>
2025-04-01 19:30:05 -0300

    Script formatting and some type hints

--- spider-ik-2d/assets/scripts/leg.gd
+++ spider-ik-2d/assets/scripts/leg.gd
@@ -3,25 +3,25 @@ class_name Leg
 
 const MIN_DISTANCE: float = 64
 
-@export var joint1:Node2D
-@export var upper_leg_sprite:Sprite2D
-@export var joint2:Node2D
-@export var mid_leg_sprite:Sprite2D
-@export var hand:Node2D
-@export var hand_sprite:Sprite2D
-@export var flipped:bool = true
-@export var point_at_mouse:bool = false
+@export var joint1: Node2D
+@export var upper_leg_sprite: Sprite2D
+@export var joint2: Node2D
+@export var mid_leg_sprite: Sprite2D
+@export var hand: Node2D
+@export var hand_sprite: Sprite2D
+@export var flipped: bool = true
+@export var point_at_mouse: bool = false
 
-var len_upper:float = 0
-var len_middle:float = 0
-var len_lower:float = 0
+var len_upper: float = 0
+var len_middle: float = 0
+var len_lower: float = 0
 
-var goal_pos:Vector2 = Vector2()
-var int_pos:Vector2 = Vector2()
-var start_pos:Vector2 = Vector2()
-var step_height:float = 40
-var step_rate:float = 0.5
-var step_time:float = 0.0
+var goal_pos: Vector2 = Vector2()
+var int_pos: Vector2 = Vector2()
+var start_pos: Vector2 = Vector2()
+var step_height: float = 40
+var step_rate: float = 0.5
+var step_time: float = 0.0
 
 func _on_ready():
 	# joints are laid out horizontally left to right
@@ -32,25 +32,25 @@ func _on_ready():
 	mid_leg_sprite.flip_h = flipped
 	hand_sprite.flip_h = flipped
 	
-func step(g_pos:Vector2):
+func step(g_pos: Vector2):
 	if goal_pos == g_pos:
 		return
 
 	goal_pos = g_pos
-	var hand_pos:Vector2 = hand.global_position
+	var hand_pos: Vector2 = hand.global_position
 
-	var highest:float = goal_pos.y
+	var highest: float = goal_pos.y
 	if hand_pos.y < highest:
 		highest = hand_pos.y
 
-	var mid:float = (goal_pos.x + hand_pos.x) / 2.0
+	var mid: float = (goal_pos.x + hand_pos.x) / 2.0
 	start_pos = hand_pos
 	int_pos = Vector2(mid, highest - step_height)
 	step_time = 0.0
 	
 func _process(delta: float) -> void:
 	step_time += delta
-	var target_pos:Vector2 = global_position
+	var target_pos: Vector2 = global_position
 	if point_at_mouse:
 		target_pos = get_global_mouse_position()
 		if target_pos < global_position:
@@ -62,7 +62,7 @@ func _process(delta: float) -> void:
 		mid_leg_sprite.flip_h = flipped
 		hand_sprite.flip_h = flipped
 	else:
-		var t:float = step_time / step_rate
+		var t: float = step_time / step_rate
 		if t < 0.5:
 			target_pos = start_pos.lerp(int_pos, t / 0.5)
 		elif t < 1.0:
@@ -74,39 +74,39 @@ func _process(delta: float) -> void:
 	update_ik(target_pos)
 	
 
-func update_ik(target_pos:Vector2):
-	var offset:Vector2 = target_pos - global_position
-	var dis_to_tar:float = offset.length()
+func update_ik(target_pos: Vector2):
+	var offset: Vector2 = target_pos - global_position
+	var dis_to_tar: float = offset.length()
 	if dis_to_tar < MIN_DISTANCE:
 		offset = (offset / dis_to_tar) * MIN_DISTANCE
 		dis_to_tar = MIN_DISTANCE
  
-	var base_r:float = offset.angle()
-	var len_total:float = len_upper + len_middle + len_lower
-	var len_dummy_side:float = (len_upper + len_middle) * clampf(dis_to_tar / len_total, 0.0, 1.0)
+	var base_r: float = offset.angle()
+	var len_total: float = len_upper + len_middle + len_lower
+	var len_dummy_side: float = (len_upper + len_middle) * clampf(dis_to_tar / len_total, 0.0, 1.0)
  
-	var base_angles:Dictionary = three_side_calc(len_dummy_side, len_lower, dis_to_tar)
-	var next_angles:Dictionary = three_side_calc(len_upper, len_middle, len_dummy_side)
+	var base_angles: Dictionary = three_side_calc(len_dummy_side, len_lower, dis_to_tar)
+	var next_angles: Dictionary = three_side_calc(len_upper, len_middle, len_dummy_side)
  
 	global_rotation = base_angles.B + next_angles.B + base_r
 	joint1.rotation = next_angles.C
 	joint2.rotation = base_angles.C + next_angles.A
 	
-func three_side_calc(side_a:float, side_b:float, side_c:float):
+func three_side_calc(side_a: float, side_b: float, side_c: float):
 	if side_c >= side_a + side_b:
 		return {"A": 0, "B": 0, "C": 0}
-	var angle_a:float = law_of_cos(side_b, side_c, side_a)
-	var angle_b:float = law_of_cos(side_c, side_a, side_b) + PI
-	var angle_c:float = PI - angle_a - angle_b
+	var angle_a: float = law_of_cos(side_b, side_c, side_a)
+	var angle_b: float = law_of_cos(side_c, side_a, side_b) + PI
+	var angle_c: float = PI - angle_a - angle_b
  
 	if flipped:
-		angle_a = -angle_a
-		angle_b = -angle_b
-		angle_c = -angle_c
+		angle_a = - angle_a
+		angle_b = - angle_b
+		angle_c = - angle_c
  
 	return {"A": angle_a, "B": angle_b, "C": angle_c}
  
 func law_of_cos(a, b, c) -> float:
 	if a == 0 or b == 0:
 		return 0
-	return acos( (pow(a, 2) + pow(b, 2) - pow(c, 2)) / ( 2 * a * b) )
+	return acos((pow(a, 2) + pow(b, 2) - pow(c, 2)) / (2 * a * b))
--- spider-ik-2d/assets/scripts/spider.gd
+++ spider-ik-2d/assets/scripts/spider.gd
@@ -1,31 +1,31 @@
 extends CharacterBody2D
 class_name Spider
 
-@export var front_check:RayCast2D
-@export var low_mid_check:RayCast2D
-@export var high_mid_check:RayCast2D
-@export var back_check:RayCast2D
-@export var forward_check:RayCast2D
+@export var front_check: RayCast2D
+@export var low_mid_check: RayCast2D
+@export var high_mid_check: RayCast2D
+@export var back_check: RayCast2D
+@export var forward_check: RayCast2D
 
-@export var front_legs_node:Node2D
-var front_legs:Array
-@export var back_legs_node:Node2D
-var back_legs:Array
+@export var front_legs_node: Node2D
+var front_legs: Array
+@export var back_legs_node: Node2D
+var back_legs: Array
 
-@export var move_to_mouse:bool = true
-@export var move_speed:float = 60
-@export var x_speed:float = 60
-@export var y_speed:float = 120
+@export var move_to_mouse: bool = true
+@export var move_speed: float = 60
+@export var x_speed: float = 60
+@export var y_speed: float = 120
 
-@export var step_rate:float = 0.2
-@export var high_check_distance:float = 86
-@export var low_check_distance:float = 24
-@export var front_check_distance:float = 128
-@export var back_check_distance:float = -8
+@export var step_rate: float = 0.2
+@export var high_check_distance: float = 86
+@export var low_check_distance: float = 24
+@export var front_check_distance: float = 128
+@export var back_check_distance: float = -8
 
-var time_since_last_step:float = 0
-var current_front_leg:int = 0
-var current_back_leg:int = 0
+var time_since_last_step: float = 0
+var current_front_leg: int = 0
+var current_back_leg: int = 0
 var use_front = false
 
 func _on_ready() -> void:
@@ -47,20 +47,20 @@ func _on_ready() -> void:
 		step()
 
 func _physics_process(delta: float) -> void:
-	var move_vec:Vector2 = Vector2(x_speed, 0)
+	var move_vec: Vector2 = Vector2(x_speed, 0)
 	if move_to_mouse:
-		var mouse_pos:Vector2 = get_global_mouse_position()
+		var mouse_pos: Vector2 = get_global_mouse_position()
 		move_vec = (mouse_pos - global_position).normalized() * move_speed
 	
 	if move_vec.x >= 0:
 		front_check.position.x = front_check_distance
 		back_check.position.x = back_check_distance
 	else:
-		front_check.position.x = -back_check_distance
-		back_check.position.x = -front_check_distance
+		front_check.position.x = - back_check_distance
+		back_check.position.x = - front_check_distance
 	
 	if high_mid_check.is_colliding() or forward_check.is_colliding():
-		move_vec.y = -y_speed
+		move_vec.y = - y_speed
 	elif not low_mid_check.is_colliding():
 		move_vec.y = y_speed
 	if forward_check.is_colliding():
@@ -68,19 +68,19 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_collide(move_vec * delta)
 
-func _process(delta):
+func _process(delta: float):
 	time_since_last_step += delta
 	if time_since_last_step >= step_rate:
 		time_since_last_step = 0
 		if move_to_mouse:
-			var mouse_pos:Vector2 = get_global_mouse_position()
+			var mouse_pos: Vector2 = get_global_mouse_position()
 			if mouse_pos.distance_to(global_position) < move_speed:
 				return
 		step()
 		
 func step():
-	var leg = null
-	var sensor = null
+	var leg: Node2D = null
+	var sensor: RayCast2D = null
 	
 	if use_front:
 		leg = front_legs[current_front_leg]
